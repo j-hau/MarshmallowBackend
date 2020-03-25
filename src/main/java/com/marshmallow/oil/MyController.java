00ba60d8abf6255a0e7066f6db668eb38bc41b82
James <james.hau@me.com>
2020-03-25 20:46:34 +0000

    Added some rough testing functions to test POST body and Jackson translation

--- src/main/java/com/marshmallow/oil/MyController.java
+++ src/main/java/com/marshmallow/oil/MyController.java
@@ -14,14 +14,29 @@ public class MyController {
     @RequestMapping(value = "cleaner", method = RequestMethod.POST)
     public Cleaning cleaner(@RequestBody OilMapping data){
         Cleaning result = new Cleaning();
+        result.setFinalPosition(new int[] {5,5});
+        result.setNumOfCleanedPatches(5);
         return result;
     }
-
 
     @RequestMapping(value = "robot-cleaner", method = RequestMethod.POST)
     public OilMapping robot_cleaner(@RequestBody OilMapping data){
         OilMapping oilMap = new OilMapping();
-        return data;
+        oilMap.setStartingPosition(data.getStartingPosition());
+        oilMap.setNavigationInstructions(data.getNavigationInstructions());
+        oilMap.setAreaSize(data.getAreaSize());
+        oilMap.setOilPatches(data.getOilPatches());
+        return oilMap;
+    }
+
+    @RequestMapping(value = "test-cleaner", method = RequestMethod.POST)
+    public OilMapping test_cleaner(@RequestBody OilMapping data){
+        OilMapping oilMap = new OilMapping();
+        oilMap.setStartingPosition(new int[] {10,10});
+        oilMap.setNavigationInstructions("TESTING VALUES");
+        oilMap.setAreaSize(new int[] {10,10});
+        oilMap.setOilPatches(new int[][] {{10,10},{9,9},{8,8},{7,7}});
+        return oilMap;
     }
 
     /**
