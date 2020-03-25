0c3ebf005a805324e67d0c54f37bb863368bea37
James <james.hau@me.com>
2020-03-25 21:38:04 +0000

    Added comments and removal of garbage.

--- src/main/java/com/marshmallow/oil/MyController.java
+++ src/main/java/com/marshmallow/oil/MyController.java
@@ -1,16 +1,17 @@
 package com.marshmallow.oil;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
-
 
 @RestController
 @RequestMapping("/home")
 public class MyController {
 
+    /**
+     * This will be the main function that does the computation.  Of type Cleaning which is in the format that
+     * is requested in the documentation.  It takes in the body
+     * @param data
+     * @return
+     */
     @RequestMapping(value = "cleaner", method = RequestMethod.POST)
     public Cleaning cleaner(@RequestBody OilMapping data){
         Cleaning result = new Cleaning();
@@ -19,6 +20,12 @@ public class MyController {
         return result;
     }
 
+    /**
+     * Test function
+     * @param data is the JSON file that is being retrieved in the body of the POST request
+     * @return oilMap is being returned which is a Java object of the JSON file input
+     * This allows me to see how the JSON file can be mapped to a Java object
+     */
     @RequestMapping(value = "robot-cleaner", method = RequestMethod.POST)
     public OilMapping robot_cleaner(@RequestBody OilMapping data){
         OilMapping oilMap = new OilMapping();
@@ -29,6 +36,12 @@ public class MyController {
         return oilMap;
     }
 
+    /**
+     * Test function
+     * @param data is the JSON file that is being retrieved in the body of the POST request
+     * @return oilMap is being returned which is a Java object of the JSON file input
+     * This acts as a test function which allows me to play with the values and see what is being returned.
+     */
     @RequestMapping(value = "test-cleaner", method = RequestMethod.POST)
     public OilMapping test_cleaner(@RequestBody OilMapping data){
         OilMapping oilMap = new OilMapping();
