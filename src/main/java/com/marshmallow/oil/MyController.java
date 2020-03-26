a651d10d900ee1b69d36691d6a6918352663c807
James <james.hau@me.com>
2020-03-26 13:40:11 +0000

    COmments

--- src/main/java/com/marshmallow/oil/MyController.java
+++ src/main/java/com/marshmallow/oil/MyController.java
@@ -21,12 +21,12 @@ public class MyController {
     }
 
     /**
-     * Test function
+     * Test function to allow me understanding oh how JSON data is processed by Jackson
      * @param data is the JSON file that is being retrieved in the body of the POST request
      * @return oilMap is being returned which is a Java object of the JSON file input
      * This allows me to see how the JSON file can be mapped to a Java object
      */
-    @RequestMapping(value = "robot-cleaner", method = RequestMethod.POST)
+    @RequestMapping(value = "robot-cleaner-doNotUse", method = RequestMethod.POST)
     public OilMapping robot_cleaner(@RequestBody OilMapping data){
         OilMapping oilMap = new OilMapping();
         oilMap.setStartingPosition(data.getStartingPosition());
@@ -37,12 +37,12 @@ public class MyController {
     }
 
     /**
-     * Test function
+     * Test function to allow me understanding oh how JSON data is processed by Jackson
      * @param data is the JSON file that is being retrieved in the body of the POST request
      * @return oilMap is being returned which is a Java object of the JSON file input
      * This acts as a test function which allows me to play with the values and see what is being returned.
      */
-    @RequestMapping(value = "test-cleaner", method = RequestMethod.POST)
+    @RequestMapping(value = "test-cleaner-doNotUse", method = RequestMethod.POST)
     public OilMapping test_cleaner(@RequestBody OilMapping data){
         OilMapping oilMap = new OilMapping();
         oilMap.setStartingPosition(new int[] {10,10});
@@ -51,11 +51,4 @@ public class MyController {
         oilMap.setOilPatches(new int[][] {{10,10},{9,9},{8,8},{7,7}});
         return oilMap;
     }
-
-    /**
-    @RequestMapping("/robotic-cleaner")
-    public OilMapping patchData(@RequestBody OilMapping inputVal){
-        return new OilMapping(inputVal);
-    }
-     **/
 }
